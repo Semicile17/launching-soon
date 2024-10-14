eba154bb403ae19f7fde3db9307efaa7804a0460
vky5 <vky05@proton.me>
2024-10-14 21:13:47 +0530

    fixed eslit issue of LandingText

--- src/components/LandingText.jsx
+++ src/components/LandingText.jsx
@@ -1,11 +1,18 @@
 import { useTheme } from "@/contexts/ThemeContext";
 
 export default function Landingtext() {
-  const {theme} = useTheme()
+  const { theme } = useTheme();
   return (
     <div>
       <span className="flex fade-in-text-large">
-        <h1 className={`${theme === 'light'?'text-black':'text-white'} font-bold md:text-4xl text-2xl font-mono`}>WE'RE</h1>
+        <h1
+          className={`${
+            theme === "light" ? "text-black" : "text-white"
+          } font-bold md:text-4xl text-2xl font-mono`}
+        >
+          WE&apos;RE
+        </h1>
+
         <h1
           className="
         text-white font-mono font-bold text-4xl 
@@ -15,29 +22,34 @@ export default function Landingtext() {
           LAUNCHING SOON !
         </h1>
       </span>
-      <br/>
+      <br />
       <span className="flex">
         <h1
-          className={`${theme === 'light'?'text-black':'text-white'}
+          className={`${theme === "light" ? "text-black" : "text-white"}
          font-mono md:text-xl fade-in-text-small
         `}
         >
-          and ready to make a 
+          and ready to make a
         </h1>
-        <h1 className="font-mono text-white text-4xl ml-2 gradient-text fade-in-text-small">CHANGE</h1>
+        <h1 className="font-mono text-white text-4xl ml-2 gradient-text fade-in-text-small">
+          CHANGE
+        </h1>
       </span>
-      <br/>
+      <br />
       <hr className="gradient md:w-[400px]"></hr>
-      <br/>
-      <span className={`${theme === 'light'?'text-black':'text-white'} fade-in-text-small`}>
-        Want to know more 
+      <br />
+      <span
+        className={`${
+          theme === "light" ? "text-black" : "text-white"
+        } fade-in-text-small`}
+      >
+        Want to know more
         <span className="ml-1 font-serif">about us</span>
-         ?
-        <br/>
-        <br/>
+        ?
+        <br />
+        <br />
         Drop your mail below
       </span>
-
     </div>
   );
 }
